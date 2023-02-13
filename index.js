b97f49dc1bc3b32a8a26e33ca55c96f4aaa7e1da
horbachoff <mr.horbachov@gmail.com>
2023-02-13 16:18:35 -0500

    code review

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ const { Joke } = require('./db');
 const { Op } = require('sequelize');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-// app.get('/jokes', async (req, res, next) => {
-//   try {
-//     // TODO - filter the jokes by tags and content
-//     const jokes = await Joke.findAll();
-//     res.send(jokes);
-//   } catch (error) {
-//     console.error(error);
-//     next(error)
-//   }
-// });
-// use where, op.like array
+
 app.get('/jokes', async (req, res, next) => {
   try {
     if(Object.keys(req.query).length===0){
@@ -39,19 +29,7 @@ app.get('/jokes', async (req, res, next) => {
     );
     res.send(jokes);
   }
-    // app.get('/jokes', async (req, res, next) => {
-    //   try {
-    //     if(Object.keys(req.query).length===0){
-    //       const jokes = await Joke.findAll({});
-    //       res.send(jokes);
-    //     }
-    //     else {
-    //       const where = {};
-    //       const jokes = await Joke.findAll(
-    //         {where:{tags:'dad'}
-    //         }
-    //       )
-    //     }
+  
   } catch (error) {
     console.error(error);
     next(error)
